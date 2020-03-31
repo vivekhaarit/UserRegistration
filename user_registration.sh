4674c251f7a208959026b12ba5ed0b5d79609251
vivek kumar <vivekhaarit@gmail.com>
2020-04-01 01:06:48 +0530

    [vivek]Added:first name validated

--- user_registration.sh
+++ user_registration.sh
@@ -2,3 +2,13 @@
  
 echo "welcome to user registration program"
 echo "your first & last name, mobile no, email will be validated"
+
+echo "enter your first name:"
+read "firstname"
+firstnamepattern="^[A-Z][a-z]{3,}"
+if [[ $firstname =~ $firstnamepattern ]]
+then
+	echo "valid first name"
+else
+	echo "invalid first name"
+fi
